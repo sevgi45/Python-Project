014e396871afe828dc13d0a4487232a03d89868e
sevgi <sevgibilen4519@gmail.com>
2023-10-26 11:50:46 +0200

    hello-world-app.py and jinja.py updated

--- flask-_01_02_Jinja_S_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_S_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,8 +1,20 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route('/')
+def head():
+    return "Hello Cohort 15"
+@app.route('/second')
+def second():
+    return "This is a Second Page "
 
+@app.route('/third')
+def third():
+    return "This is my third Page"   
 
+@app.route('/<string:id>')
+def forth(id):
+    return f'Id of this page is {id}'
 
 
 
